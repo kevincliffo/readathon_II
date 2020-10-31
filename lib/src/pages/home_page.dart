173d14a0848ee6fcd6eeed8f0a27cba0adcc9a09
Kevin Ochieng <kevincliffo@gmail.com>
2020-11-01 01:44:38 +0300

    Reworked home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,147 +1,81 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:readathon/models/channel_info.dart';
-import 'package:readathon/models/videos_list.dart';
-import 'package:readathon/src/screens/video_player_screen.dart';
-import 'package:readathon/src/utils/services.dart';
 
 class HomePage extends StatefulWidget {
+  static String tag = 'home-page';
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  ChannelInfo _channelInfo;
-  VideosList _videosList;
-  Item _item;
-  bool _loading;
-  String _playListId;
-  String _nextPageToken;
-  ScrollController _scrollController;
 
   @override
   void initState() { 
-    super.initState();
-    _loading = true;
-    _nextPageToken = '';
-    _scrollController = ScrollController();
-    _videosList = VideosList();
-    _videosList.videos = List();
-    _getChannelInfo();    
-  }
-
-  _getChannelInfo()async{
-    _channelInfo = await Services.getChannelInfo();
-    _item = _channelInfo.items[0];
-    _playListId = _item.contentDetails.relatedPlaylists.uploads;
-    
-    await _loadVideos();
-    setState(() {
-      _loading = false;
-    });
-  }
-
-  _loadVideos() async{
-    VideosList tempVideosList = await Services.getVideosList(
-      playListId:_playListId,
-      pageToken:_nextPageToken,
-    );
-
-    _nextPageToken = tempVideosList.nextPageToken;
-    _videosList.videos.addAll(tempVideosList.videos);
-    setState(() {
-      
-    });
+    super.initState();   
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-       appBar: AppBar(
-         title:Text(_loading?'Loading...':"E-Funza Videos"),
-       ),
-       body:Container(
-         color:Colors.white,
-         child: Column(
-           children:[
-            _buildInfoView(),
-            Expanded(
-              child: NotificationListener<ScrollEndNotification>(
-                onNotification: (ScrollNotification notification) {
-                  if(_videosList.videos.length >= int.parse(_item.statistics.videoCount)){
-                    return true;
-                  }
-                  if(notification.metrics.pixels == notification.metrics.maxScrollExtent){
-                    _loadVideos();
-                  }
-                  return true;
-                },
-                child: ListView.builder(
-                  controller: _scrollController,
-                  itemCount: _videosList.videos.length,
-                  itemBuilder: (context, index){
-                    VideoItem videoItem = _videosList.videos[index];
-                    return InkWell(
-                      onTap: ()async{
-                        Navigator.push(
-                          context, MaterialPageRoute(builder: (context){
-                            return VideoPlayerScreen(videoItem: videoItem,);
-                          }
-                          )
-                        );
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(20.0),
-                        child:Row(
-                          children: [
-                            CachedNetworkImage(
-                              imageUrl: videoItem.video.thumbnails.thumbnailsDefault.url,
-                            ),
-                            SizedBox(width:20.0),
-                            Flexible(child: Text(videoItem.video.title)),
-                            SizedBox(width:20.0),
-                          ],                  
-                        )
-                      ),
-                    );
-                  }),
-              ),
-             ),
-           ],
-         ),
-       )
-    );
-  }
-
-  _buildInfoView(){
-    return _loading 
-      ? CircularProgressIndicator()
-      :Container(
-        child:Card(
-          child:Row(
-            children:[
-              CircleAvatar(
-                backgroundImage: CachedNetworkImageProvider(
-                  _item.snippet.thumbnails.medium.url,
-                ),
-              ),
-              SizedBox(width:20.0),
-              Expanded(
-                child: Text(
-                  _item.snippet.title,
-                  style:TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                  )
-                ),
-              ),
-              Text(
-                _item.statistics.videoCount,
-              ),
-              SizedBox(width:20.0),
-            ],
-          ),
+    final profilePic = Hero(
+      tag: 'hero',
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: CircleAvatar(
+          radius: 72.0,
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage('assets/images/e-funza2.png'),
         ),
+      ),
+    );
+
+    final welcome = Padding(
+      padding: EdgeInsets.all(8.0),
+      child:Text(
+        'E-Funza',
+        style:TextStyle(fontSize: 28.0, color:Colors.black),
+      ),
+    );
+
+    final bio = Padding(
+      padding: EdgeInsets.all(8.0),
+      child:Text(
+        'Google Flutter developer Kenya',
+        textAlign: TextAlign.center,
+        style:TextStyle(fontSize: 16.0, color:Colors.black),
+      ),
+    );
+
+    final fav = Padding(
+      padding: EdgeInsets.all(8.0),
+      child:Text(
+        'For God so loved the world, that he gave his only son, tht whosoever beieveth in him should not perish but have everlasting life',
+        textAlign: TextAlign.center,
+        style:TextStyle(fontSize: 16.0, color:Colors.black),
+      ),
+    );
+
+    final body = Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(28.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors:[
+            Colors.yellow[200],
+            Colors.yellow[200],
+          ]
+        ),
+      ),
+      child:Column(
+        children: <Widget>[
+          profilePic,
+          welcome,
+          bio,
+          fav
+        ],
+      )
+    );
+
+    return Scaffold(
+      body: body,
     );
   }
 }
--- lib/src/pages/videos_page.dart
+++ lib/src/pages/videos_page.dart
@@ -1,17 +1,147 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:readathon/models/channel_info.dart';
+import 'package:readathon/models/videos_list.dart';
+import 'package:readathon/src/screens/video_player_screen.dart';
+import 'package:readathon/src/utils/services.dart';
 
 class VideosPage extends StatefulWidget {
-  VideosPage({Key key}) : super(key: key);
-
   @override
   _VideosPageState createState() => _VideosPageState();
 }
 
 class _VideosPageState extends State<VideosPage> {
+  ChannelInfo _channelInfo;
+  VideosList _videosList;
+  Item _item;
+  bool _loading;
+  String _playListId;
+  String _nextPageToken;
+  ScrollController _scrollController;
+
+  @override
+  void initState() { 
+    super.initState();
+    _loading = true;
+    _nextPageToken = '';
+    _scrollController = ScrollController();
+    _videosList = VideosList();
+    _videosList.videos = List();
+    _getChannelInfo();    
+  }
+
+  _getChannelInfo()async{
+    _channelInfo = await Services.getChannelInfo();
+    _item = _channelInfo.items[0];
+    _playListId = _item.contentDetails.relatedPlaylists.uploads;
+    
+    await _loadVideos();
+    setState(() {
+      _loading = false;
+    });
+  }
+
+  _loadVideos() async{
+    VideosList tempVideosList = await Services.getVideosList(
+      playListId:_playListId,
+      pageToken:_nextPageToken,
+    );
+
+    _nextPageToken = tempVideosList.nextPageToken;
+    _videosList.videos.addAll(tempVideosList.videos);
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       //child: child,
+    return Scaffold(
+       appBar: AppBar(
+         title:Text(_loading?'Loading...':"E-Funza Videos"),
+       ),
+       body:Container(
+         color:Colors.white,
+         child: Column(
+           children:[
+            _buildInfoView(),
+            Expanded(
+              child: NotificationListener<ScrollEndNotification>(
+                onNotification: (ScrollNotification notification) {
+                  if(_videosList.videos.length >= int.parse(_item.statistics.videoCount)){
+                    return true;
+                  }
+                  if(notification.metrics.pixels == notification.metrics.maxScrollExtent){
+                    _loadVideos();
+                  }
+                  return true;
+                },
+                child: ListView.builder(
+                  controller: _scrollController,
+                  itemCount: _videosList.videos.length,
+                  itemBuilder: (context, index){
+                    VideoItem videoItem = _videosList.videos[index];
+                    return InkWell(
+                      onTap: ()async{
+                        Navigator.push(
+                          context, MaterialPageRoute(builder: (context){
+                            return VideoPlayerScreen(videoItem: videoItem,);
+                          }
+                          )
+                        );
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(20.0),
+                        child:Row(
+                          children: [
+                            CachedNetworkImage(
+                              imageUrl: videoItem.video.thumbnails.thumbnailsDefault.url,
+                            ),
+                            SizedBox(width:20.0),
+                            Flexible(child: Text(videoItem.video.title)),
+                            SizedBox(width:20.0),
+                          ],                  
+                        )
+                      ),
+                    );
+                  }),
+              ),
+             ),
+           ],
+         ),
+       )
+    );
+  }
+
+  _buildInfoView(){
+    return _loading 
+      ? CircularProgressIndicator()
+      :Container(
+        child:Card(
+          child:Row(
+            children:[
+              CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(
+                  _item.snippet.thumbnails.medium.url,
+                ),
+              ),
+              SizedBox(width:20.0),
+              Expanded(
+                child: Text(
+                  _item.snippet.title,
+                  style:TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  )
+                ),
+              ),
+              Text(
+                _item.statistics.videoCount,
+              ),
+              SizedBox(width:20.0),
+            ],
+          ),
+        ),
     );
   }
 }

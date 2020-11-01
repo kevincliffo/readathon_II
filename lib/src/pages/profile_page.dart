33fd9c3190b833c8358659264e9b8ea3605b1872
Kevin Ochieng <kevincliffo@gmail.com>
2020-11-01 03:30:51 +0300

    Added widgets for Profile Page

--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -1,17 +1,181 @@
 import 'package:flutter/material.dart';
+import 'package:readathon/src/widgets/custom_list_tile.dart';
 
-class ProfilePage extends StatefulWidget {
-  ProfilePage({Key key}) : super(key: key);
-
-  @override
-  _ProfilePageState createState() => _ProfilePageState();
+class ProfilePage extends StatefulWidget{
+  _ProfilePageState createState()=> _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage>
+{
+  bool turnOnNotification = false;
+  bool turnOnLocation = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //  child: child,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal:20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:<Widget>[
+              Text("Profile", 
+                style:TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height:20.0),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height:120.0,
+                    width:120,
+                    decoration:BoxDecoration(
+                      borderRadius: BorderRadius.circular(60),
+                      boxShadow:[
+                        BoxShadow(
+                          blurRadius:3.0,
+                          offset:Offset(0, 4.0),
+                          color:Colors.black12
+                        ),
+                      ],
+                      image:DecorationImage(
+                        image: AssetImage("assets/images/e-funza2.png"),
+                        fit:BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width:20.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Prof. John Doe", style:TextStyle(fontSize:16.0)),
+                    SizedBox(height:10.0),
+                    Text("0724113312", style:TextStyle(color: Colors.grey)),
+                    SizedBox(height:10.0),
+                    // SmallButton(btnText:"Edit"),
+                  ],
+                ),
+              ],
+              ),
+              SizedBox(height:30.0),
+              Text("Account", 
+                style:TextStyle(
+                  fontSize:20, fontWeight:FontWeight.bold
+                )
+              ),
+              SizedBox(height:20),
+              Card(
+                elevation:5.0,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child:Column(
+                    children: <Widget>[
+                      CustomListTile(
+                        icon:Icons.location_on, 
+                        text:"Location"
+                      ),
+                      Divider(height:10.0, color:Colors.grey,),
+                      CustomListTile(
+                        icon:Icons.visibility, 
+                        text:"Change Password"
+                      ),
+                      Divider(height:10.0, color:Colors.grey,),
+                      // CustomListTile(
+                      //   icon:Icons.shopping_cart, 
+                      //   text:"Shipping"
+                      // ),
+                      Divider(height:10.0, color:Colors.grey,),
+                      CustomListTile(
+                        icon:Icons.payment, 
+                        text:"Payment"
+                      ),
+                      Divider(height:10.0, color:Colors.grey,),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height:20),
+              Text("Notifications", 
+                style:TextStyle(
+                  fontSize:20, fontWeight:FontWeight.bold
+                )
+              ),
+              SizedBox(height:20),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children:<Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "App Notification"
+                          ),
+                          Switch(
+                            value: turnOnNotification, 
+                            onChanged: (bool value){                            
+                              setState(() {
+                                turnOnNotification = value;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                      Divider(height:10.0, color:Colors.grey,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Location Tracking"
+                          ),
+                          Switch(
+                            value: turnOnLocation, 
+                            onChanged: (bool value){                            
+                              setState(() {
+                                turnOnLocation = value;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                      Divider(height:10.0, color:Colors.grey,),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height:20),
+              Text("Other", 
+                style:TextStyle(
+                  fontSize:20, fontWeight:FontWeight.bold
+                )
+              ),
+              SizedBox(height:20),
+              Card(
+                child:Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child:Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Language", style:TextStyle(fontSize:16.0)),
+                        Divider(height:30.0, color:Colors.grey,),
+                        Text("Currency", style:TextStyle(fontSize:16.0)),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
